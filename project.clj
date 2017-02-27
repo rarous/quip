26a46e6f973dc2a3bd84a1d2f4078c0db4a6ed22
Aleš Roubíček <ales@roubicek.name>
2017-02-27 14:08:08 +0100

    Bumped transit dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject rarous/quip "0.1.1"
+(defproject rarous/quip "0.1.2-SNAPSHOT"
   :description "Clojure client for Quip APIs"
   :url "https://github.com/rarous/quip"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.cognitect/transit-clj "0.8.288"]
+  :dependencies [[com.cognitect/transit-clj "0.8.297"]
                  [http-kit "2.2.0"]]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :scm {:name "git" :url "https://github.com/rarous/quip"}
